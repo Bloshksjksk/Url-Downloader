5c028aab0dc96f7ec58e674ebd217bb4fc64987f
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-05-27 18:34:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/Jegaa1/Url-Downloader /Url-Downloader
+cd /Url-Downloader
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
