ee2b4501195ebfe0adde6fe2c422282289e5d2d3
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-26 19:21:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,1 @@
-echo "Cloning Repo...."
-git clone https://github.com/Bloshksjksk/Url-Downloader /Url-Downloader
-cd /Url-Downloader
-pip3 install -r requirements.txt
-echo "Starting Bot...."
 python3 bot.py
